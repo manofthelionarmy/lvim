238baeec2bbc8c5f4dac80905e90c5e5e2c2b141
armando <armando@pop-os.localdomain>
2023-04-08 13:13:03 -0700

    Added and configured emmet_ls

--- config.lua
+++ config.lua
@@ -347,3 +347,23 @@ require('modules/icons')
 --     require("nvim-treesitter.highlight").attach(0, "bash")
 --   end,
 -- })
+-- Emmet is part of the skipped servers. I have to configure it myself, or remove it from skipped servers list
+-- https://www.lunarvim.org/docs/troubleshooting#update-node
+local lspconfig = require('lspconfig')
+local configs = require('lspconfig/configs')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.emmet_ls.setup({
+  -- on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+  init_options = {
+    html = {
+      options = {
+        -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+        ["bem.enabled"] = true,
+      },
+    },
+  }
+})
